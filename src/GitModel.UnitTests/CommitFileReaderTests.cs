1d0779d103b09a080976c3b7326c1b64b84c18ab
Alex Novak <alexwnovak@gmail.com>
2017-10-28 13:40:52 -0400

    CommitFileReader now throws on bad file path

--- src/GitModel.UnitTests/CommitFileReaderTests.cs
+++ src/GitModel.UnitTests/CommitFileReaderTests.cs
@@ -1,8 +1,29 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
+using FluentAssertions;
 
 namespace GitModel.UnitTests
 {
    public class CommitFileReaderTests
    {
+      [Fact]
+      public void FromFile_FilePathIsNull_ThrowsArgumentException()
+      {
+         var commitFileReader = new CommitFileReader();
+
+         Action fromFile = () => commitFileReader.FromFile( null );
+
+         fromFile.ShouldThrow<ArgumentException>();
+      }
+
+      [Fact]
+      public void FromFile_FilePathIsEmptyString_ThrowsArgumentException()
+      {
+         var commitFileReader = new CommitFileReader();
+
+         Action fromFile = () => commitFileReader.FromFile( string.Empty );
+
+         fromFile.ShouldThrow<ArgumentException>();
+      }
    }
 }
--- src/GitModel/CommitFileReader.cs
+++ src/GitModel/CommitFileReader.cs
@@ -1,6 +1,17 @@
-﻿namespace GitModel
+﻿using System;
+
+namespace GitModel
 {
    public class CommitFileReader
    {
+      public void FromFile( string filePath )
+      {
+         if ( string.IsNullOrEmpty( filePath ) )
+         {
+            throw new ArgumentException( "File path must not be null or empty", nameof( filePath ) );
+         }
+
+         throw new NotImplementedException();
+      }
    }
 }
