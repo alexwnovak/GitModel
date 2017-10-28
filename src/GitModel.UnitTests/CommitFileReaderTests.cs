403832aa75db1f71f1e84b2bed8409d3ba3990fb
Alex Novak <alexwnovak@gmail.com>
2017-10-28 14:34:36 -0400

    Now throws FileNotFound if the file path doesn't exist

--- src/GitModel.UnitTests/CommitFileReaderTests.cs
+++ src/GitModel.UnitTests/CommitFileReaderTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using Xunit;
 using FluentAssertions;
+using Moq;
 
 namespace GitModel.UnitTests
 {
@@ -9,7 +11,7 @@ namespace GitModel.UnitTests
       [Fact]
       public void FromFile_FilePathIsNull_ThrowsArgumentException()
       {
-         var commitFileReader = new CommitFileReader();
+         var commitFileReader = new CommitFileReader( Mock.Of<IFileSystem>() );
 
          Action fromFile = () => commitFileReader.FromFile( null );
 
@@ -19,11 +21,26 @@ namespace GitModel.UnitTests
       [Fact]
       public void FromFile_FilePathIsEmptyString_ThrowsArgumentException()
       {
-         var commitFileReader = new CommitFileReader();
+         var commitFileReader = new CommitFileReader( Mock.Of<IFileSystem>() );
 
          Action fromFile = () => commitFileReader.FromFile( string.Empty );
 
          fromFile.ShouldThrow<ArgumentException>();
+      }
+
+      [Fact]
+      public void FromFile_FilePathDoesNotExist_ThrowsFileNotFoundException()
+      {
+         const string filePath = "DoesntMatter";
+
+         var fileSystemMock = new Mock<IFileSystem>();
+         fileSystemMock.Setup( fs => fs.FileExists( filePath ) ).Returns( false );
+
+         var commitFileReader = new CommitFileReader( fileSystemMock.Object );
+
+         Action fromFile = () => commitFileReader.FromFile( filePath );
+
+         fromFile.ShouldThrow<FileNotFoundException>();
       }
    }
 }
--- src/GitModel/CommitFileReader.cs
+++ src/GitModel/CommitFileReader.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.IO;
 
 namespace GitModel
 {
    public class CommitFileReader
    {
+      private readonly IFileSystem _fileSystem;
+
+      public CommitFileReader( IFileSystem fileSystem )
+      {
+         _fileSystem = fileSystem;
+      }
+
       public void FromFile( string filePath )
       {
          if ( string.IsNullOrEmpty( filePath ) )
          {
             throw new ArgumentException( "File path must not be null or empty", nameof( filePath ) );
+         }
+
+         if ( !_fileSystem.FileExists( filePath ) )
+         {
+            throw new FileNotFoundException( $"Couldn't find file: {filePath}" );
          }
 
          throw new NotImplementedException();
