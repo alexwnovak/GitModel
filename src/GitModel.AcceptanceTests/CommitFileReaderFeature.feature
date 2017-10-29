9552e913a091df8dae67add97277820c0ee2422f
Alex Novak <alexwnovak@gmail.com>
2017-10-29 09:40:08 -0400

    +Feature for CommitFileReader

---
+++ src/GitModel.AcceptanceTests/CommitFileReaderFeature.feature
@@ -0,0 +1,10 @@
+﻿Feature: CommitFileReader
+   As a commit editor writer
+   I want to read Git commit files
+   So I can allow the user to edit their commit message
+
+@Acceptance
+Scenario: Can read subject from commit file
+   Given the commit file has subject "This is the subject"
+   When I read the commit file
+   Then the subject should be "This is the subject"
---
+++ src/GitModel.AcceptanceTests/Steps/CommitFileReaderSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace GitModel.AcceptanceTests.Steps
+{
+   [Binding]
+   public class CommitFileReaderSteps
+   {
+      [Given( @"the commit file has subject ""(.*)""" )]
+      public void GivenTheCommitFileHasSubject( string subject )
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [When( @"I read the commit file" )]
+      public void WhenIReadTheCommitFile()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then( @"the subject should be ""(.*)""" )]
+      public void ThenTheSubjectShouldBe( string subject )
+      {
+         ScenarioContext.Current.Pending();
+      }
+   }
+}
