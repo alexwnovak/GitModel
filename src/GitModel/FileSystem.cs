4a52e62f50bc6417eb7f9470a1237da6fbb37dd1
Alex Novak <alexwnovak@gmail.com>
2017-10-30 18:30:38 -0400

    +FileSystem concrete implementation

---
+++ src/GitModel/FileSystem.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace GitModel
+{
+   public class FileSystem : IFileSystem
+   {
+      public bool FileExists( string filePath ) => File.Exists( filePath );
+
+      public string[] ReadAllLines( string filePath ) => File.ReadAllLines( filePath );
+   }
+}
