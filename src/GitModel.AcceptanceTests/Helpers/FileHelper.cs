597b51f4a5d43d46e1f1e19d5d69b6afb9465e6b
Alex Novak <alexwnovak@gmail.com>
2017-10-30 18:21:46 -0400

    +FileHelper for acceptance tests
    We're going full integration end-to-end, so we need to place files and clean them up.

---
+++ src/GitModel.AcceptanceTests/Helpers/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace GitModel.AcceptanceTests.Helpers
+{
+   internal static class FileHelper
+   {
+      public static void DeleteFile( string path )
+      {
+         if ( File.Exists( path ) )
+         {
+            File.Delete( path );
+         }
+      }
+
+      public static string WriteTempFile( params string[] contents )
+      {
+         string tempFileName = Path.GetTempFileName();
+
+         File.WriteAllLines( tempFileName, contents );
+
+         return tempFileName;
+      }
+   }
+}
