34f98516741a079c11572986408bbbb91c53a5ec
Alex Novak <alexwnovak@gmail.com>
2017-10-31 12:51:45 -0400

    Basic structure for a CommitFileWriter feature

---
+++ src/GitModel.AcceptanceTests/CommitFileWriterFeature.feature
@@ -0,0 +1,10 @@
+﻿Feature: CommitFileWriter
+   As a commit editor writer
+   I want to write Git commit files
+   So I can give the user's commit to Git for committing
+
+@Acceptance
+Scenario: Can write subject to commit file
+   Given the commit document subject is "This is the subject"
+   When I write commit file
+   Then the commit file subject is "This is the subject"
---
+++ src/GitModel.AcceptanceTests/Steps/CommitFileWritersteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace GitModel.AcceptanceTests.Steps
+{
+   [Binding]
+   public class CommitFileWriterSteps
+   {
+      [Given( @"the commit document subject is ""(.*)""" )]
+      public void GivenTheCommitDocumentSubjectIs( string subject )
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [When( @"I write commit file" )]
+      public void WhenIWriteCommitFile()
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then( @"the commit file subject is ""(.*)""" )]
+      public void ThenTheCommitFileSubjectIs( string expectedSubject )
+      {
+         ScenarioContext.Current.Pending();
+      }
+   }
+}
