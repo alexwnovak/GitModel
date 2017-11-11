750ed3ff92d6e81533b28209f7b5f7fe4fde6dc2
Alex Novak <alexwnovak@gmail.com>
2017-11-11 12:14:42 -0500

    Extracting out a shared step transform for string -> string[]

---
+++ src/GitModel.AcceptanceTests/Transformations/StringTransformations.cs
@@ -0,0 +1,12 @@
+﻿using TechTalk.SpecFlow;
+
+namespace GitModel.AcceptanceTests.Transformations
+{
+   [Binding]
+   public class StringTransformations
+   {
+      [StepArgumentTransformation]
+      public string[] TransformToListOfString( string commaSeparatedList ) =>
+         commaSeparatedList.Split( ',' );
+   }
+}
