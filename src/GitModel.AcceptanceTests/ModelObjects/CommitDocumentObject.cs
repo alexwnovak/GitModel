324d61a6c75ea0d5559c8dcd49ab1761c2329060
Alex Novak <alexwnovak@gmail.com>
2017-11-11 14:09:11 -0500

    +CommitDocumentObject to act as a "page object" for CommitDocument
    This encapsulates a nice API for tests to talk to for a one-stop shop in the document. It also packs away test details into an object and not tests.

---
+++ src/GitModel.AcceptanceTests/ModelObjects/CommitDocumentObject.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using GitModel;
+
+namespace GitModel.AcceptanceTests.ModelObjects
+{
+   internal class CommitDocumentObject
+   {
+      private readonly string _tempFileName = Path.GetTempFileName();
+      private CommitDocument _commitDocument = new CommitDocument();
+
+      public string Subject
+      {
+         get => _commitDocument.Subject;
+         set => _commitDocument.Subject = value;
+      }
+
+      public string[] Body
+      {
+         get => _commitDocument.Body;
+         set => _commitDocument.Body = value;
+      }
+
+      public void Load()
+      {
+         var commitFileReader = new CommitFileReader();
+
+         _commitDocument = commitFileReader.FromFile( _tempFileName );
+      }
+
+      public void Save()
+      {
+         var commitFileWriter = new CommitFileWriter();
+
+         commitFileWriter.ToFile( _tempFileName, _commitDocument );
+      }
+   }
+}
