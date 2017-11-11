5484d03f46c8961e18d7ae7df1af48486a7ce3c9
Alex Novak <alexwnovak@gmail.com>
2017-11-11 16:11:14 -0500

    Ditching writer and reader steps since they're now shared

--- src/GitModel.AcceptanceTests/Steps/Keys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GitModel.AcceptanceTests.Steps
-{
-   internal static class Keys
-   {
-      public const string CommitFilePathKey = "CommitFilePath";
-      public const string CommitDocumentKey = "CommitDocument";
-      public const string CommitFileHelperKey = "CommitFileHelper";
-   }
-}
