6f5e5dcc38e92d99f96f9993a0ce00a36132b680
Alex Novak <alexwnovak@gmail.com>
2017-11-12 16:17:25 -0500

    Not sure what I was thinking with those first comments

--- src/GitModel/ICommitFileReader.cs
+++ src/GitModel/ICommitFileReader.cs
@@ -6,10 +6,17 @@ namespace GitModel
    /// This class can parse Git commit files from disk. It can read the subject line, as well as
    /// secondary notes (the body).
    /// </summary>
-   /// <param name="filePath">The path to the Git commit file on disk. This must not be null or empty</param>
-   /// <exception cref="ArgumentException">Thrown if filePath is null or empty.</exception>
    public interface ICommitFileReader
    {
+      /// <summary>
+      /// Reads a Git commit file from disk.
+      /// </summary>
+      /// <param name="filePath">The full path to the Git commit file. This must not be null or empty.</param>
+      /// <returns>A <seealso cref="CommitDocument"/> object that contains the commit details.</returns>
+      /// <exception cref="ArgumentException">Thrown if filePath is null or empty.</exception>
+      /// <exception cref="GitModelException">
+      /// Thrown if disk access fails for any reason. Refer to the inner exception for details.
+      /// </exception>
       CommitDocument FromFile( string filePath );
    }
 }
