d1742c1024b08ebf8c3c972091166d4d4d21dada
Alex Novak <alexwnovak@gmail.com>
2017-11-12 16:04:39 -0500

    +Xml docs for CommitFileReader

--- src/GitModel/CommitFileReader.cs
+++ src/GitModel/CommitFileReader.cs
@@ -6,6 +6,12 @@ using GitModel.Internal;
 
 namespace GitModel
 {
+   /// <summary>
+   /// This class can parse Git commit files from disk. It can read the subject line, as well as
+   /// secondary notes (the body).
+   /// </summary>
+   /// <param name="filePath">The path to the Git commit file on disk. This must not be null or empty</param>
+   /// <exception cref="ArgumentException">Thrown if filePath is null or empty.</exception>
    public class CommitFileReader : ICommitFileReader
    {
       private readonly IFileSystem _fileSystem;
--- src/GitModel/ICommitFileReader.cs
+++ src/GitModel/ICommitFileReader.cs
@@ -1,5 +1,13 @@
-﻿namespace GitModel
+﻿using System;
+
+namespace GitModel
 {
+   /// <summary>
+   /// This class can parse Git commit files from disk. It can read the subject line, as well as
+   /// secondary notes (the body).
+   /// </summary>
+   /// <param name="filePath">The path to the Git commit file on disk. This must not be null or empty</param>
+   /// <exception cref="ArgumentException">Thrown if filePath is null or empty.</exception>
    public interface ICommitFileReader
    {
       CommitDocument FromFile( string filePath );
