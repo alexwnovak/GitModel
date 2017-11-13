e3b829c7a3985e44422392f585d5eb06c490e448
Alex Novak <alexwnovak@gmail.com>
2017-11-12 22:27:50 -0500

    Can now match against "pick"

--- src/GitModel.UnitTests/Internal/RebaseActionParserTests.cs
+++ src/GitModel.UnitTests/Internal/RebaseActionParserTests.cs
@@ -1,6 +1,18 @@
-﻿namespace GitModel.UnitTests.Internal
+﻿using Xunit;
+using FluentAssertions;
+using GitModel;
+using GitModel.Internal;
+
+namespace GitModel.UnitTests.Internal
 {
    public class RebaseActionParserTests
    {
+      [Theory]
+      [InlineData( "pick", RebaseAction.Pick )]
+      [InlineData( "p", RebaseAction.Pick )]
+      public void ToRebaseAction_ActionStringIsValid_ConvertsToRebaseAction( string actionString, RebaseAction expectedAction )
+      {
+         RebaseActionParser.ToRebaseAction( actionString ).Should().Be( expectedAction );
+      }
    }
 }
--- src/GitModel/Internal/RebaseActionParser.cs
+++ src/GitModel/Internal/RebaseActionParser.cs
@@ -1,6 +1,18 @@
-﻿namespace GitModel.Internal
+﻿using System;
+
+namespace GitModel.Internal
 {
    internal static class RebaseActionParser
    {
+      public static RebaseAction ToRebaseAction( string actionString )
+      {
+         actionString = actionString.ToLower();
+
+         if ( actionString == "pick" || actionString == "p" )
+         {
+            return RebaseAction.Pick;
+         }
+         throw new NotImplementedException();
+      }
    }
 }
