8b16ad25e29b29ef7a57926a19f3f0d24f5f1312
Alex Novak <alexwnovak@gmail.com>
2017-11-12 22:12:44 -0500

    +RebaseDocument to collect a series of rebase items

---
+++ src/GitModel/RebaseDocument.cs
@@ -0,0 +1,17 @@
+﻿namespace GitModel
+{
+   /// <summary>
+   /// 
+   /// </summary>
+   public class RebaseDocument
+   {
+      /// <summary>
+      /// 
+      /// </summary>
+      public RebaseItem[] Items
+      {
+         get;
+         set;
+      }
+   }
+}
