544a3755e6dc2b20deead1b481e6c8a234733e6b
Alex Novak <alexwnovak@gmail.com>
2017-11-12 21:52:08 -0500

    +FromFile stub that rejects blank file paths

--- src/GitModel.UnitTests/RebaseFileReaderTests.cs
+++ src/GitModel.UnitTests/RebaseFileReaderTests.cs
@@ -1,6 +1,21 @@
-﻿namespace GitModel.UnitTests
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace GitModel.UnitTests
 {
    public class RebaseFileReaderTests
    {
+      [Theory]
+      [InlineData( null )]
+      [InlineData( "" )]
+      public void FromFile_FilePathIsInvalid_ThrowsArgumentException( string filePath )
+      {
+         var rebaseFileReader = new RebaseFileReader();
+
+         Action fromFile = () => rebaseFileReader.FromFile( filePath );
+
+         fromFile.ShouldThrow<ArgumentException>();
+      }
    }
 }
--- src/GitModel/RebaseFileReader.cs
+++ src/GitModel/RebaseFileReader.cs
@@ -1,6 +1,20 @@
-﻿namespace GitModel
+﻿using System;
+
+namespace GitModel
 {
+   /// <summary>
+   /// 
+   /// </summary>
    public class RebaseFileReader
    {
+      public void FromFile( string filePath )
+      {
+         if ( string.IsNullOrEmpty( filePath ) )
+         {
+            throw new ArgumentException( "File path must not be null or empty", nameof( filePath ) );
+         }
+
+         throw new NotImplementedException();
+      }
    }
 }
