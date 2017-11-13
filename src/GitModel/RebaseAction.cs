1d6f8772e40be2c66ae85f810bf671a5b4fe4098
Alex Novak <alexwnovak@gmail.com>
2017-11-12 22:26:52 -0500

    +Stub XML comments for rebase action enum values

--- src/GitModel/RebaseAction.cs
+++ src/GitModel/RebaseAction.cs
@@ -5,11 +5,34 @@
    /// </summary>
    public enum RebaseAction
    {
+      /// <summary>
+      /// 
+      /// </summary>
       Pick,
+
+      /// <summary>
+      /// 
+      /// </summary>
       Reword,
+
+      /// <summary>
+      /// 
+      /// </summary>
       Edit,
+
+      /// <summary>
+      /// 
+      /// </summary>
       Squash,
+
+      /// <summary>
+      /// 
+      /// </summary>
       Fixup,
+
+      /// <summary>
+      /// 
+      /// </summary>
       Exec
    }
 }
