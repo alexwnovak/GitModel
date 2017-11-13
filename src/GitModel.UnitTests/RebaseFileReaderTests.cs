64883a058eede7dc3351ae692d38c832663e5cbc
Alex Novak <alexwnovak@gmail.com>
2017-11-12 22:21:13 -0500

    First step in the rebase reading algorithm for a simple case
    The action-to-enum needs to support the short-hand (p for pick as well). Also needs to support subjects with spaces.

--- src/GitModel.UnitTests/RebaseFileReaderTests.cs
+++ src/GitModel.UnitTests/RebaseFileReaderTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Xunit;
 using FluentAssertions;
+using Moq;
+using GitModel.Internal;
 
 namespace GitModel.UnitTests
 {
@@ -11,11 +13,41 @@ namespace GitModel.UnitTests
       [InlineData( "" )]
       public void FromFile_FilePathIsInvalid_ThrowsArgumentException( string filePath )
       {
-         var rebaseFileReader = new RebaseFileReader();
+         var rebaseFileReader = new RebaseFileReader( Mock.Of<IFileSystem>() );
 
          Action fromFile = () => rebaseFileReader.FromFile( filePath );
 
          fromFile.ShouldThrow<ArgumentException>();
+      }
+
+      [Fact]
+      public void FromFile_RebaseFileHasOneItem_ReadsItem()
+      {
+         const string filePath = "RebaseFile.txt";
+         var lines = new[]
+         {
+            "pick d768f10 WIP"
+         };
+
+         // Arrange
+
+         var fileSystemMock = new Mock<IFileSystem>();
+         fileSystemMock.Setup( fs => fs.ReadAllLines( filePath ) ).Returns( lines );
+
+         // Act
+
+         var rebaseFileReader = new RebaseFileReader( fileSystemMock.Object );
+
+         var rebaseDocument = rebaseFileReader.FromFile( filePath );
+
+         // Assert
+
+         rebaseDocument.Items.Should()
+            .HaveCount( 1 )
+            .And
+            .Contain( i => i.Action == RebaseAction.Pick
+               && i.CommitHash == "d768f10"
+               && i.Subject == "WIP" );
       }
    }
 }
--- src/GitModel/RebaseFileReader.cs
+++ src/GitModel/RebaseFileReader.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using GitModel.Internal;
 
 namespace GitModel
 {
@@ -7,14 +9,46 @@ namespace GitModel
    /// </summary>
    public class RebaseFileReader
    {
-      public void FromFile( string filePath )
+      private IFileSystem _fileSystem;
+
+      internal RebaseFileReader( IFileSystem fileSystem )
+      {
+         _fileSystem = fileSystem;
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <param name="filePath"></param>
+      /// <returns></returns>
+      public RebaseDocument FromFile( string filePath )
       {
          if ( string.IsNullOrEmpty( filePath ) )
          {
             throw new ArgumentException( "File path must not be null or empty", nameof( filePath ) );
          }
 
-         throw new NotImplementedException();
+         var allLines = _fileSystem.ReadAllLines( filePath );
+         var rebaseItems = new List<RebaseItem>();
+
+         foreach ( string line in allLines )
+         {
+            var tokens = line.Split( ' ' );
+
+            var rebaseItem = new RebaseItem
+            {
+               Action = (RebaseAction) Enum.Parse( typeof( RebaseAction ), tokens[0], true ),
+               CommitHash = tokens[1],
+               Subject = tokens[2]
+            };
+
+            rebaseItems.Add( rebaseItem );
+         }
+
+         return new RebaseDocument
+         {
+            Items = rebaseItems.ToArray()
+         };
       }
    }
 }
