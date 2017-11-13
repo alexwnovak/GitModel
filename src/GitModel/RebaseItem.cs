dfaea8ffd20b814b6c9630daec567334c2b61c2d
Alex Novak <alexwnovak@gmail.com>
2017-11-12 22:12:33 -0500

    +Rebase item to capture all info for one line in a rebase file

---
+++ src/GitModel/RebaseItem.cs
@@ -0,0 +1,35 @@
+﻿namespace GitModel
+{
+   /// <summary>
+   /// 
+   /// </summary>
+   public class RebaseItem
+   {
+      /// <summary>
+      /// 
+      /// </summary>
+      public RebaseAction Action
+      {
+         get;
+         set;
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      public string CommitHash
+      {
+         get;
+         set;
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      public string Subject
+      {
+         get;
+         set;
+      }
+   }
+}
