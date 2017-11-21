90c3e2d8923390a3867d61e781335333b0fe703d
Alex Novak <alexwnovak@gmail.com>
2017-11-20 22:32:49 -0500

    +RebaseDocumentObject to model a rebase document

---
+++ src/GitModel.AcceptanceTests/ModelObjects/RebaseDocumentObject.cs
@@ -0,0 +1,13 @@
+﻿namespace GitModel.AcceptanceTests.ModelObjects
+{
+   public class RebaseDocumentObject
+   {
+      private readonly RebaseDocument _rebaseDocument = new RebaseDocument();
+
+      public RebaseItem[] RebaseItems
+      {
+         get => _rebaseDocument.Items;
+         set => _rebaseDocument.Items = value;
+      }
+   }
+}
