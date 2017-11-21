acb9bdf4826b6131026f1339439ef1091b90e950
Alex Novak <alexwnovak@gmail.com>
2017-11-20 22:39:37 -0500

    Fleshing out the rest of the acceptance tests

--- src/GitModel.AcceptanceTests/ModelObjects/RebaseDocumentObject.cs
+++ src/GitModel.AcceptanceTests/ModelObjects/RebaseDocumentObject.cs
@@ -1,13 +1,24 @@
-﻿namespace GitModel.AcceptanceTests.ModelObjects
+﻿using System.IO;
+
+namespace GitModel.AcceptanceTests.ModelObjects
 {
    public class RebaseDocumentObject
    {
+      private readonly string _tempFileName = Path.GetTempFileName();
       private readonly RebaseDocument _rebaseDocument = new RebaseDocument();
 
-      public RebaseItem[] RebaseItems
+      public RebaseItem[] Items
       {
          get => _rebaseDocument.Items;
          set => _rebaseDocument.Items = value;
+      }
+
+      public void Load()
+      {
+      }
+
+      public void Save()
+      {
       }
    }
 }
--- src/GitModel.AcceptanceTests/Steps/RebaseSteps.cs
+++ src/GitModel.AcceptanceTests/Steps/RebaseSteps.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Linq;
+using FluentAssertions;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using GitModel.AcceptanceTests.ModelObjects;
 
 namespace GitModel.AcceptanceTests.Steps
 {
    [Binding]
    public class RebaseSteps
    {
-      private readonly RebaseDocument _rebaseDocument;
+      private readonly RebaseDocumentObject _rebaseDocument;
 
-      public RebaseSteps( RebaseDocument rebaseDocument )
+      public RebaseSteps( RebaseDocumentObject rebaseDocument )
       {
          _rebaseDocument = rebaseDocument;
       }
@@ -16,21 +20,27 @@ namespace GitModel.AcceptanceTests.Steps
       [Given( @"the rebase has the following:" )]
       public void GivenTheRebaseHasTheFollowingData( Table table )
       {
+         _rebaseDocument.Items = table.CreateSet<RebaseItem>().ToArray();
       }
 
       [Given( @"the rebase file exists" )]
       public void GivenTheRebaseFileExists()
       {
+         _rebaseDocument.Save();
       }
 
       [When( @"I read the rebase file" )]
       public void WhenIReadTheRebaseFile()
       {
+         _rebaseDocument.Load();
       }
 
       [Then( @"the rebase document should contain:" )]
       public void ThenTheRebaseDocumentShouldContain( Table table )
       {
+         var expectedItems = table.CreateSet<RebaseItem>();
+
+         _rebaseDocument.Items.Should().BeEquivalentTo( expectedItems );
       }
    }
 }
