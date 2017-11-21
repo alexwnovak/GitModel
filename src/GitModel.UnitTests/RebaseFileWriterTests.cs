47239f361419c6ce77003beee3314c31a4a42b30
Alex Novak <alexwnovak@gmail.com>
2017-11-21 12:43:18 -0500

    Trivial rejection tests for path

--- src/GitModel.UnitTests/RebaseFileWriterTests.cs
+++ src/GitModel.UnitTests/RebaseFileWriterTests.cs
@@ -1,6 +1,23 @@
-﻿namespace GitModel.UnitTests
+﻿using System;
+using FluentAssertions;
+using GitModel.Internal;
+using Moq;
+using Xunit;
+
+namespace GitModel.UnitTests
 {
    public class RebaseFileWriterTests
    {
+      [Theory]
+      [InlineData( null )]
+      [InlineData( "" )]
+      public void ToFile_FilePathIsInvalid_ThrowsArgumentException( string filePath )
+      {
+         var rebaseFileWriter = new RebaseFileWriter( Mock.Of<IFileSystem>() );
+
+         Action toFile = () => rebaseFileWriter.ToFile( filePath );
+
+         toFile.ShouldThrow<ArgumentException>();
+      }
    }
 }
--- src/GitModel/RebaseFileWriter.cs
+++ src/GitModel/RebaseFileWriter.cs
@@ -1,9 +1,28 @@
-﻿namespace GitModel
+﻿using System;
+using GitModel.Internal;
+
+namespace GitModel
 {
    /// <summary>
    /// 
    /// </summary>
    public class RebaseFileWriter
    {
+      private readonly IFileSystem _fileSystem;
+
+      internal RebaseFileWriter( IFileSystem fileSystem )
+      {
+         _fileSystem = fileSystem;
+      }
+
+      public void ToFile( string filePath )
+      {
+         if ( string.IsNullOrEmpty( filePath ) )
+         {
+            throw new ArgumentException( "File path must not be null or empty", nameof( filePath ) );
+         }
+
+         throw new System.NotImplementedException();
+      }
    }
 }
