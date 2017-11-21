80a652cfba8dbda0ca5013534e92e92a5a0dcd51
Alex Novak <alexwnovak@gmail.com>
2017-11-20 21:55:04 -0500

    +Helper class to create a string from a series of values

---
+++ src/GitModel.UnitTests/Helpers/RebaseItemHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace GitModel.UnitTests.Helpers
+{
+   internal static class RebaseItemHelper
+   {
+      public static string Create( RebaseAction action, string commitHash, string subject )
+      {
+         var rebaseItem = new RebaseItem
+         {
+            Action = action,
+            CommitHash = commitHash,
+            Subject = subject
+         };
+
+         return rebaseItem.ToString();
+      }
+   }
+}
