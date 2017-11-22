b188a67c69e805f329023f682893a41c9a4a9eb5
Alex Novak <alexwnovak@gmail.com>
2017-11-21 22:34:25 -0500

    Putting the XML docs from the interface onto the concrete

--- src/GitModel/RebaseFileWriter.cs
+++ src/GitModel/RebaseFileWriter.cs
@@ -6,14 +6,14 @@ using GitModel.Internal;
 namespace GitModel
 {
    /// <summary>
-   /// 
+   /// This class can write Git rebase files to disk.
    /// </summary>
    public class RebaseFileWriter : IRebaseFileWriter
    {
       private readonly IFileSystem _fileSystem;
 
       /// <summary>
-      /// 
+      /// Initializes a new instance of the RebaseFileWriter class.
       /// </summary>
       public RebaseFileWriter() : this( new FileSystem() )
       {
@@ -24,6 +24,22 @@ namespace GitModel
          _fileSystem = fileSystem;
       }
 
+      /// <summary>
+      /// Writes a <seealso cref="RebaseDocument"/> instance to the given file path.
+      /// </summary>
+      /// <param name="filePath">
+      /// The full path and file name to write the <seealso cref="RebaseDocument"/> instance. This must
+      /// not be null or empty.</param>
+      /// <param name="document">
+      /// The object that contains the rebase's details. This must not be null, and its contents
+      /// must not be null.
+      /// </param>
+      /// <exception cref="ArgumentException">
+      /// Thrown if either the filePath or document arguments are null.
+      /// </exception>
+      /// <exception cref="GitModelException">
+      /// Thrown if disk access fails for any reason. Refer to the inner exception for details.
+      /// </exception>
       public void ToFile( string filePath, RebaseDocument document )
       {
          if ( string.IsNullOrEmpty( filePath ) )
