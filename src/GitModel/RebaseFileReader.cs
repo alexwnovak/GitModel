c3a20be9ea0738a3e311221382fc51bacc7b6023
Alex Novak <alexwnovak@gmail.com>
2017-11-21 22:28:49 -0500

    Putting the interface XML comments on the concrete

--- src/GitModel/RebaseFileReader.cs
+++ src/GitModel/RebaseFileReader.cs
@@ -5,14 +5,15 @@ using GitModel.Internal;
 namespace GitModel
 {
    /// <summary>
-   /// 
+   /// This class can parse Git rebase files from disk. It can read each line and read the
+   /// rebase action (pick, squash, etc.), the commit hash, and the commit subject.
    /// </summary>
    public class RebaseFileReader : IRebaseFileReader
    {
       private readonly IFileSystem _fileSystem;
 
       /// <summary>
-      /// 
+      /// Initializes a new instance of the RebaseFileReader class.
       /// </summary>
       public RebaseFileReader() : this( new FileSystem() )
       {
@@ -24,10 +25,14 @@ namespace GitModel
       }
 
       /// <summary>
-      /// 
+      /// Reads a Git rebase file from disk and returns its details in a <seealso cref="RebaseDocument"/> instance.
       /// </summary>
-      /// <param name="filePath"></param>
-      /// <returns></returns>
+      /// <param name="filePath">The full path to the Git rebase file. This must not be null or empty.</param>
+      /// <returns>A <seealso cref="RebaseDocument"/> object that contains the rebase details.</returns>
+      /// <exception cref="ArgumentException">Thrown if filePath is null or empty.</exception>
+      /// <exception cref="GitModelException">
+      /// Thrown if disk access fails for any reason. Refer to the inner exception for details.
+      /// </exception>
       public RebaseDocument FromFile( string filePath )
       {
          if ( string.IsNullOrEmpty( filePath ) )
