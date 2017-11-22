fc595a67da1e4aa5f3bf7f030f34ed18ebcd0402
Alex Novak <alexwnovak@gmail.com>
2017-11-21 22:43:26 -0500

    +XML docs for rebase document

--- src/GitModel/RebaseDocument.cs
+++ src/GitModel/RebaseDocument.cs
@@ -1,12 +1,12 @@
 ﻿namespace GitModel
 {
    /// <summary>
-   /// 
+   /// Represents the data contained in a Git rebase.
    /// </summary>
    public class RebaseDocument
    {
       /// <summary>
-      /// 
+      /// A collection of items that represent the commits and how they're being applied.
       /// </summary>
       public RebaseItem[] Items
       {
